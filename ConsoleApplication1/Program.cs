e2ae446b0338eb3e390510accf691254dc710a06
Rex He <rex.he@readingroom.com.au>
2015-06-15 13:19:17 +1000

    Add inheritance

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
     internal class Program
     {
         private static DateTime dataTime;
-        private static String t1;
+        private static string t1;
+        private static string result = "empty string";
 
         private static void Main(string[] args)
         {
@@ -24,20 +22,20 @@ namespace ConsoleApplication1
 
             #region linq
 
-            int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7};
+            int[] numbers = {1, 2, 3, 4, 5, 6, 7};
 
 
             Console.WriteLine(numbers.Where(new test().Predicate).Take(3).Sum());
-            Console.WriteLine(numbers.Count(i=>i%2==0));
+            Console.WriteLine(numbers.Count(i => i%2 == 0));
 
             #endregion
 
             #region decimal 
 
-            double x = 5.0;
+            var x = 5.0;
 
-            int y = 5;
-            decimal z = new decimal(5.000);
+            var y = 5;
+            var z = new decimal(5.000);
 
 
             float f = 10;
@@ -57,21 +55,42 @@ namespace ConsoleApplication1
 
             #endregion
 
-            
-
             #region await sync
 
             var task = SaySomething(); //This is where the action start in a new thread.
 
-            Thread.Sleep(2000); //if the current main thread running longer than the new thread. It is wasted of the new thread. 
+            Thread.Sleep(2000);
+                //if the current main thread running longer than the new thread. It is wasted of the new thread. 
 //            Thread.Sleep(1000); //if the current main thread running longer than the new thread. It is wasted of the new thread. 
 
             Console.WriteLine("DOING OTHER THING. NO DELAY.");
             Console.WriteLine(result);
 
             task.Wait(); // Same as task.Result, rather no need to introduce variable.
-            
+
             Console.WriteLine(result);
+
+            #endregion
+
+            #region inheritance
+
+            var f1 = new Foo();
+            f1.M();
+
+            Foo b = new Bar();
+            b.M();
+
+            var b1 = new Bar();
+            b1.M();
+
+            A a = new A();
+            Console.WriteLine(a);
+            B bb = new B();
+            Console.WriteLine(bb);
+            Console.WriteLine("TEST Method" + bb.test());
+            A bbb = new B();
+            Console.WriteLine(bbb);
+            Console.WriteLine("TEST Method" + bbb.test());
 
             #endregion
 
@@ -79,11 +98,9 @@ namespace ConsoleApplication1
             Console.ReadLine();
         }
 
-        private static string result = "empty string";
-
         private static async Task<string> SaySomething()
         {
-            await System.Threading.Tasks.Task.Delay(1500);
+            await Task.Delay(1500);
             result = "Hello world!";
             return "Something";
         }
@@ -116,4 +133,58 @@ namespace ConsoleApplication1
             return op(radius, radius);
         }
     }
+
+    internal class Foo
+    {
+        public void M()
+        {
+            Console.WriteLine("Foo.M");
+        }
+    }
+
+    internal class Bar : Foo
+    {
+        public new void M()
+        {
+            Console.WriteLine("Bar.M");
+        }
+    }
+
+    class A
+    {
+        /*public virtual string ToString() Won't work
+        {
+            return "A";
+        }*/
+        public override string ToString()
+        {
+            return "A";
+        }
+
+        public string test()
+        {
+            return "A";
+        }
+    }
+
+    class B : A
+    {
+        /*public virtual string ToString()
+        {
+            return "B";
+        }*/
+
+        public override string ToString()
+        {
+            return "B";
+        }
+
+        public string test()
+        {
+            return "B";
+        }
+    }
+
+    
+
 }
